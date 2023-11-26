e67143278c2108fd5e50f482c4e6b7b37da6d1f5
Miluum <imiluum@gmail.com>
2023-11-26 20:03:03 +0100

    feat: add glowing tools

---
+++ src/main/java/com/miluum/glowingtoolsforge/GlowingPickaxeItem.java
@@ -0,0 +1,10 @@
+package com.miluum.glowingtoolsforge;
+
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tier;
+
+public class GlowingPickaxeItem extends PickaxeItem {
+    public GlowingPickaxeItem(Tier pTier, int pAttackDamage, float pAttackSpeed, Properties pProperties) {
+        super(pTier, pAttackDamage, pAttackSpeed, pProperties);
+    }
+}
---
+++ src/main/java/com/miluum/glowingtoolsforge/GlowingShovelItem.java
@@ -0,0 +1,10 @@
+package com.miluum.glowingtoolsforge;
+
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.Tier;
+
+public class GlowingShovelItem extends ShovelItem {
+    public GlowingShovelItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+}
