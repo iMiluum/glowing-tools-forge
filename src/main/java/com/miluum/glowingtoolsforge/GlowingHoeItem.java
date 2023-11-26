b86c6e8a1dc4238eb72ed5d22d0b020905021980
Miluum <imiluum@gmail.com>
2023-11-26 23:51:52 +0100

    feat: ported glowing tools to forge

--- src/main/java/com/miluum/glowingtoolsforge/GlowingHoeItem.java
+++ src/main/java/com/miluum/glowingtoolsforge/GlowingHoeItem.java
@@ -1,10 +1,37 @@
 package com.miluum.glowingtoolsforge;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
-public class GlowingHoeItem extends HoeItem {
+public class GlowingHoeItem extends HoeItem implements GlowingItem {
     public GlowingHoeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext context) {
+        ItemStack stack = context.getItemInHand();
+        Level world = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        Player miner = context.getPlayer();
+        Direction side = context.getClickedFace();
+
+        assert miner != null;
+        if (miner.isCrouching()) GlowingItemTransformer.transformGlowingTool(stack, world, pos, miner, side);
+        else super.useOn(context);
+
+        return InteractionResult.SUCCESS;
+    }
+
+    public boolean isGlowing(ItemStack stack) {
+        return stack.getItem() instanceof GlowingItem;
     }
 }
--- src/main/java/com/miluum/glowingtoolsforge/GlowingShovelItem.java
+++ src/main/java/com/miluum/glowingtoolsforge/GlowingShovelItem.java
@@ -1,10 +1,37 @@
 package com.miluum.glowingtoolsforge;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
-public class GlowingShovelItem extends ShovelItem {
+public class GlowingShovelItem extends ShovelItem implements GlowingItem {
     public GlowingShovelItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext context) {
+        ItemStack stack = context.getItemInHand();
+        Level world = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        Player miner = context.getPlayer();
+        Direction side = context.getClickedFace();
+
+        assert miner != null;
+        if (miner.isCrouching()) GlowingItemTransformer.transformGlowingTool(stack, world, pos, miner, side);
+        else super.useOn(context);
+
+        return InteractionResult.SUCCESS;
+    }
+
+    public boolean isGlowing(ItemStack stack) {
+        return stack.getItem() instanceof GlowingItem;
     }
 }
